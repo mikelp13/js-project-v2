1831ef42f4e07ab2fa9f4e990002046219f66e58
julia_chizhik <for.yu.ch@gmail.com>
2020-12-23 19:15:12 +0200

    Summary

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,1 +1,13 @@
 import './styles.css';
+
+
+$(function () {
+        $(document).on('click touchstart', '.category-card', function(){ 
+            modalOpen();
+            console.log('card click', $(this));
+        });
+    });
+
+function modalOpen () { 
+    // refs.modalwindow.classList.add('is-open');
+};
