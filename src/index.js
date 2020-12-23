d6e9d472b8f569654be65527e063a37d824ff096
Iryna Kosynienko <ikosynienko@gmail.com>
2020-12-23 20:26:23 +0100

    add old files

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import './components/developers/modal-window/dev-modal';
 import './components/catalog/categories-list-item/index.js';
 import './components/auth/accCabinet';
 // import './components/spinner/spin';
+import './components/product-info/index.js'
 import './components/header/js/newAdv';
 import './components/ads/newAddForm/newAddForm.js';
 
