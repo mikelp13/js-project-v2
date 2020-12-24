f5ae6a912e8e6dd8f986c636debb6036a4af5183
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2020-12-25 00:16:34 +0200

    added random slides to slider

--- src/components/hero/slider/slider.js
+++ src/components/hero/slider/slider.js
@@ -13,7 +13,8 @@ const refs = {
 };
 
 const createSliderMarkup = data => {
-  refs.slider.insertAdjacentHTML('afterbegin', sliderTemplate(data));
+  const randomSlides = _.shuffle(data) 
+  refs.slider.insertAdjacentHTML('afterbegin', sliderTemplate(randomSlides));
 };
 
 const createItemsMarkup = data => {
@@ -71,88 +72,3 @@ const getSliderItems = async () => {
 };
 
 getSliderItems();
-
-//===
-
-// -------------------------------------- test -----------------------------------------------
-// const items = [
-//   {
-//     title: 'Smart Watch',
-//     price: 799,
-//     imageUrl:
-//       './images/hero-img/watch.png',
-//   },
-//   {
-//     title: 'New T-Shirt',
-//     price: 699,
-//     imageUrl:
-//       'https://storage.googleapis.com/kidslikev2_bucket/bdefcbc72735f64db17f3250b1e64245.png',
-//   },
-//   {
-//     title: 'Nikon Camera',
-//     price: 3499,
-//     imageUrl:
-//       'https://storage.googleapis.com/kidslikev2_bucket/photo_camera_PNG7834.png',
-//   },
-//   {
-//     title: 'Apple Monitor',
-//     price: 8999,
-//     imageUrl:
-//       'https://storage.googleapis.com/kidslikev2_bucket/580b57fbd9996e24bc43bfe2.png',
-//   },
-//   {
-//     title: 'Old School Radio',
-//     price: 975,
-//     imageUrl:
-//       'https://storage.googleapis.com/kidslikev2_bucket/58ac44f7f86c9c2eea74c4e6.png',
-//   },
-//   {
-//     title: 'Compact Stereo Speakers',
-//     price: 850,
-//     imageUrl:
-//       'https://storage.googleapis.com/kidslikev2_bucket/z120-stereo-speakers.png',
-//   },
-//   {
-//     title: 'Flying Drone 42K black',
-//     price: 4000,
-//     imageUrl:
-//       'https://storage.googleapis.com/kidslikev2_bucket/5cb84c5f7ff3656569c8cec5.png',
-//   },
-//   {
-//     title: 'Powerful Black Speakers',
-//     price: 999,
-//     imageUrl:
-//       'https://storage.googleapis.com/kidslikev2_bucket/Speaker-PNG-Background-Image.png',
-//   },
-//   {
-//     title: 'Pink Beat Headphones',
-//     price: 599,
-//     imageUrl:
-//       'https://storage.googleapis.com/kidslikev2_bucket/580b57fbd9996e24bc43bfba.png',
-//   },
-//   {
-//     title: 'Vintage Microphone and Stand',
-//     price: 2400,
-//     imageUrl:
-//       'https://storage.googleapis.com/kidslikev2_bucket/580b57fbd9996e24bc43bfdb.png',
-//   },
-// ];
-
-// function createMarkup(items) {
-//   const sliderItems = items.reduce(
-//     (acc, { title, price, imageUrl }) => {
-//       acc += `<li class="slider__item">
-//       <div class="slider__description">
-//         <p class="slider__title">${title}</p>
-//         <p class="slider__price">${price}</p>
-//       </div>
-//       <img class="slider__img" src="${imageUrl}" alt="${title}">
-//     </li>`;
-//       return acc;
-//     },
-//     ""
-//   );
-//   return sliderItems;
-// }
-
-// refs.slider.insertAdjacentHTML('afterbegin', createMarkup(items));
