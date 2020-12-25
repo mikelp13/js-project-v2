d64e25360bdd821ff8520977a9b09697e61bfb12
ka2nq1 <ka2nq1@gmail.com>
2020-12-25 14:01:44 +0200

    asd

--- src/components/header/js/color-switch.js
+++ src/components/header/js/color-switch.js
@@ -1,41 +1,56 @@
+// ===================================REFS==================================
 const orangeBtn = document.querySelector('.orange-btn');
 const blueBtn = document.querySelector('.blue-btn');
 const html = document.getElementsByTagName('html')[0];
-
-const logetUserIcon = document.querySelectorAll('#loggedUser__exit:hover')
-console.log(logetUserIcon);
-
+const headerLogo = document.querySelector(".logo-orange");
+const headerLogoMobile = document.querySelector(".logo-orange-mobile");
+const headerAuth = document.querySelector(".header-auth")
+const closeMenu = document.querySelector('.close-menu-orange')
+const mobileBurger = document.querySelector('.mobile-burger-orange')
+const mobileFilterBtn = document.querySelector('.header__filters--btn-orange')
+const mobileClearFilterBtn = document.querySelector('.header__clear-filter--btn-orange')
+const filterBtn = document.querySelector('.header__btn-orange')
+const clearFilterBtn = document.querySelector('.header__clear-btn-orange')
+const tabletInputBtn = document.querySelector('.tablet-input-btn-orange')
+const deskInputBtn = document.querySelector('.desk-input-btn-orange')
+const logedUserCard = document.querySelector('.loggedUser__carts-orange')
+const exitUserCard = document.querySelector('.loggedUser__exit-orange')
+const mobilelogedUserCard = document.querySelector('.mobile-loggedUser__carts-orange')
+const mobileexitUserCard = document.querySelector('.mobile-loggedUser__exit-orange')
+// ===================================REFS==================================
+// =================================LISTENERS==============================
 blueBtn.addEventListener('change', () => {
-    // html.setAttribute("style", "--user-exit-color: url(./components/header/images/logout-blue.svg)");
-    // html.style.setProperty("--logo-color", "url(./components/header/images/magic-logo/header-magic-logo-blue.svg)");
     html.style.setProperty("--main-accent-color", "#00bcd4");
-    // html.style.setProperty("--main-accent-color", "#00bcd4");
-    // html.style.setProperty("--main-accent-color", "#00bcd4");
-    // html.style.setProperty("--main-accent-color", "#00bcd4");
-    // html.style.setProperty("--main-accent-color", "#00bcd4");
-    // html.style.setProperty("--main-accent-color", "#00bcd4");
-    // html.style.setProperty("--main-accent-color", "#00bcd4");
+    headerLogo.classList.toggle("blue");
+    headerLogoMobile.classList.toggle("blue");
+    closeMenu.classList.toggle("close-menu-blue");
+    mobileBurger.classList.toggle("mobile-burger-blue");
+    mobileFilterBtn.classList.toggle("header__filters--btn-blue");
+    mobileClearFilterBtn.classList.toggle("header__clear-filter--btn-blue");
+    filterBtn.classList.toggle("header__filters--btn-blue");
+    clearFilterBtn.classList.toggle("header__clear-filter--btn-blue");
+    tabletInputBtn.classList.toggle("search-input-btn-blue");
+    deskInputBtn.classList.toggle("search-input-btn-blue");
+    logedUserCard.classList.toggle("loggedUser__carts-blue");
+    exitUserCard.classList.toggle("loggedUser__exit-blue");
+    mobilelogedUserCard.classList.toggle("loggedUser__carts-blue");
+    mobileexitUserCard.classList.toggle("loggedUser__exit-blue");
 })
-
 orangeBtn.addEventListener('change', () => {
     html.style.setProperty("--main-accent-color", "#ff6b08");
+    headerLogo.classList.remove("blue");
+    headerLogoMobile.classList.remove("blue");
+    closeMenu.classList.remove("close-menu-blue");
+    mobileBurger.classList.remove("mobile-burger-blue");
+    mobileFilterBtn.classList.remove("header__filters--btn-blue");
+    mobileClearFilterBtn.classList.remove("header__clear-filter--btn-blue");
+    filterBtn.classList.remove("header__filters--btn-blue");
+    clearFilterBtn.classList.remove("header__clear-filter--btn-blue");
+    tabletInputBtn.classList.remove("search-input-btn-blue");
+    deskInputBtn.classList.remove("search-input-btn-blue");
+    logedUserCard.classList.remove("loggedUser__carts-blue");
+    exitUserCard.classList.remove("loggedUser__exit-blue");
+    mobilelogedUserCard.classList.remove("loggedUser__carts-blue");
+    mobileexitUserCard.classList.remove("loggedUser__exit-blue");
 })
-
-//   --user-exit-color: url(./components/header/images/logout-orange.svg);
-//   --user-cards-color: url(./components/header/images/user-profile-orange.svg);
-//   --burger-menu-color: url(./components/menu/icons/dehaze-24px-orange.svg);
-//   --clear-mobile-color: url(./components/refresh-filtres/restore/replay-24px-orange.svg);
-//   --filters-mobile-color: url(./components/refresh-filtres/filters/tune-24px-orange.svg);
-//   --close-color: url(./components/header/images/close/close-24px-orange.svg);
-//   --search-color: url(./components/search/icons/search-24px-orange.svg);
-//   --logo-color: url(./components/header/images/magic-logo/header-magic-logo.svg);
-
-
-
-
-// function colorSwither() {
-//     if (blueBtn.check) {
-//         return html.style.setProperty("--main-accent-color", "#00bcd4");
-//     }
-// }
-// colorSwither()
+// =================================LISTENERS==============================
--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ import './components/developers/js/index';
 import './components/developers/modal-window/dev-modal';
 import './components/catalog/categories-list-item/index.js';
 import './components/auth/accCabinet';
-// import './components/spinner/spin';
 import './components/header/js/color-switch';
+// import './components/spinner/spin';
 import './components/product-info/index.js'
 import './components/header/js/newAdv';
 import './components/search/index';
