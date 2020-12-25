1394a39cdceb696d2ce2f624da964609d6174d05
ka2nq1 <ka2nq1@gmail.com>
2020-12-25 10:35:22 +0200

    s

--- src/components/header/js/color-switch.js
+++ src/components/header/js/color-switch.js
@@ -1,15 +1,41 @@
 const orangeBtn = document.querySelector('.orange-btn');
 const blueBtn = document.querySelector('.blue-btn');
-// const orange = document.querySelectorAll('.orange')
-const universalSelector = document.getElementsByTagName("body");
+const html = document.getElementsByTagName('html')[0];
 
-// console.log(orange);
-function colorSwither() {
-    const orange = document.getElementsByClassName('orange')
-    for (let i = 0; i < orange.length; i++) {
-     orange[i].classList.add('blue');
-        orange[i].classList.remove('orange');
-    }
-}
+const logetUserIcon = document.querySelectorAll('#loggedUser__exit:hover')
+console.log(logetUserIcon);
 
-colorSwither()
+blueBtn.addEventListener('change', () => {
+    // html.setAttribute("style", "--user-exit-color: url(./components/header/images/logout-blue.svg)");
+    // html.style.setProperty("--logo-color", "url(./components/header/images/magic-logo/header-magic-logo-blue.svg)");
+    html.style.setProperty("--main-accent-color", "#00bcd4");
+    // html.style.setProperty("--main-accent-color", "#00bcd4");
+    // html.style.setProperty("--main-accent-color", "#00bcd4");
+    // html.style.setProperty("--main-accent-color", "#00bcd4");
+    // html.style.setProperty("--main-accent-color", "#00bcd4");
+    // html.style.setProperty("--main-accent-color", "#00bcd4");
+    // html.style.setProperty("--main-accent-color", "#00bcd4");
+})
+
+orangeBtn.addEventListener('change', () => {
+    html.style.setProperty("--main-accent-color", "#ff6b08");
+})
+
+//   --user-exit-color: url(./components/header/images/logout-orange.svg);
+//   --user-cards-color: url(./components/header/images/user-profile-orange.svg);
+//   --burger-menu-color: url(./components/menu/icons/dehaze-24px-orange.svg);
+//   --clear-mobile-color: url(./components/refresh-filtres/restore/replay-24px-orange.svg);
+//   --filters-mobile-color: url(./components/refresh-filtres/filters/tune-24px-orange.svg);
+//   --close-color: url(./components/header/images/close/close-24px-orange.svg);
+//   --search-color: url(./components/search/icons/search-24px-orange.svg);
+//   --logo-color: url(./components/header/images/magic-logo/header-magic-logo.svg);
+
+
+
+
+// function colorSwither() {
+//     if (blueBtn.check) {
+//         return html.style.setProperty("--main-accent-color", "#00bcd4");
+//     }
+// }
+// colorSwither()
