a481d77cf983ce2fed3b91ead0d4e5cf6c0385cc
Yurii Berezynets <yuriiberezynets@gmail.com>
2020-12-26 13:34:36 +0200

    fix favouriteApi logic

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,7 @@
-import { data } from '../data/data';
 import axios from 'axios';
+import { data } from '../data/data';
 import { camelCase } from 'lodash';
 const token = JSON.parse(localStorage.getItem('accessToken'));
-console.log(token);
 axios.defaults.baseURL = 'https://callboard-backend.herokuapp.com';
 axios.defaults.headers.common['Authorization'] = token;
 
@@ -62,41 +61,17 @@ export const getAllCategories = async page => {
   }
 };
 
-//===
-// const newObjAdv = {
-//   title: '',
-//   description: '',
-//   category: 'transport',
-//   price: '',
-//   phone: '',
-//   file: [],
-// };
-
 export const postAdv = () => {
-  return axios.post(`/call`, newObjAdv);
+  return axios.post(`/call`);
 };
-
-//===
-
-// const pathObjAdv = {
-//   title: '',
-//   description: '',
-//   category: 'transport',
-//   price: '',
-//   phone: '',
-//   file: [],
-// };
 
 export const patchAdv = id => {
-  return axios.patch(`/call/${id}`, patchObjAdv);
+  return axios.patch(`/call/${id}`);
 };
-//===
 
 export const delAdv = id => {
   return axios.delete(`/call/${id}`);
 };
-
-//===
 
 export const getUserAdv = async () => {
   try {
@@ -112,55 +87,17 @@ export const getUserAdv = async () => {
   }
 };
 
-//===
-
-export const addInFavourites = async (id, card) => {
+export const getUserData = async () => {
   try {
-    if (data.user.favourites.length) {
-      return data.user.favourites;
-    } else {
-      const res = await axios.post(`/call/favourite/${id}`, card);
-      data.user.favourites = [...res.data.user.newFavourites];
-      return res.data.newFavourites;
+    if (localStorage.getItem('accessToken')) {
+      const res = await axios.get('/user');
+      data.user = { ...res.data };
+      return res.data;
     }
   } catch (error) {
     console.log(error);
   }
 };
-
-//===
-
-export const delInFavourites = async id => {
-  try {
-    if (data.user.favourites.length) {
-      return data.user.favourites;
-    } else {
-      const res = await axios.delete(`/call/favourite/${id}`);
-      data.user.favourites = [...res.data.newFavourites];
-      return res.data.newFavourites;
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-//===
-
-export const getFavourites = async () => {
-  try {
-    if (data.user.favourites.length) {
-      return data.user.favourites;
-    } else {
-      const res = await axios.get(`/call/favourites`);
-      data.user.favourites = [...res.data.favourites];
-      return res.data.favourites;
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-//===
 
 export const getAds = async () => {
   try {
@@ -176,4 +113,64 @@ export const getAds = async () => {
   }
 };
 
-//===
+export const getFavourites = async () => {
+  try {
+    if (data.user.favourites.length) {
+      return data.user.favourites;
+    } else {
+      const res = await axios.get(`/call/favourites`);
+      data.user.favourites = [...res.data.favourites];
+      console.log('db fav:', res.data.favourites);
+      console.log('data fav:', data.user.favourites);
+      return res.data.favourites;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+getFavourites();
+
+export const favouriteApi = async (id, favorItem, favorName) => {
+  try {
+    if (localStorage.getItem('accessToken')) {
+      if (data.user.favourites.some(item => item._id === id)) {
+        await axios.delete(`/call/favourite/${id}`);
+        data.user.favourites = [
+          ...data.user.favourites.filter(item => item._id !== id),
+        ];
+        favorItem.classList.toggle('activeicon');
+        favorName.classList.toggle('activename');
+      } else {
+        const res = await axios.post(`/call/favourite/${id}`);
+        data.user.favourites = [...res.data.newFavourites];
+        favorItem.classList.toggle('activeicon');
+        favorName.classList.toggle('activename');
+      }
+    } else {
+      if (localStorage.getItem('favorites')) {
+        const localFavorites = JSON.parse(localStorage.getItem('favorites'));
+        if (
+          JSON.parse(localStorage.getItem('favorites')).some(
+            item => item === id,
+          )
+        ) {
+          localStorage.setItem(
+            'favorites',
+            JSON.stringify(localFavorites.filter(item => item !== id)),
+          );
+          favorItem.classList.toggle('activeicon');
+        } else {
+          localStorage.setItem(
+            'favorites',
+            JSON.stringify([...localFavorites, id]),
+          );
+          favorItem.classList.toggle('activeicon');
+        }
+      } else {
+        localStorage.setItem('favorites', JSON.stringify([id]));
+      }
+    }
+  } catch (error) {
+    throw error;
+  }
+};
