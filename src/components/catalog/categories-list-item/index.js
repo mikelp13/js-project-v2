45c1724ba88944a8d67253c99c2be3c0ed2c30f4
julia_chizhik <for.yu.ch@gmail.com>
2020-12-27 08:24:38 +0200

    update

--- src/components/catalog/categories-list-item/index.js
+++ src/components/catalog/categories-list-item/index.js
@@ -1,7 +1,63 @@
+// import './styles.css';
+// import { data } from "../../../data/data";
+// import categoryTemplate from './template/category.hbs';
+// import { runLoader } from '../../loader/index';
+
+
+// export const runSlider = (i) => {
+//          $(`.sliders-slick${i}`).slick({
+//             dots: true,
+//             variableWidth: true,
+//             slidesToShow: 4,
+//             slidesToScroll: 1,
+//             speed: 500,
+//             easing: 'ease',
+//             infinite: true,
+//             autoplay: true,
+//             autoplaySpeed: 3500, 
+//         });
+// };
+
+// export const createMarkup = (i) => {
+
+//     const categoryRefs = document.querySelector('.container-categories');
+//     const markup = `<div class="category visible"><div class="category-header"><h2 class="category-title">${data.calls.russianCategories[i]}
+//             </h2><a href="" class="category-link" data-category="category">Смотреть все</a></div>
+//             <ul class="category-list sliders sliders-slick${i}"> 
+//             ${categoryTemplate(data.calls.specificCategory[data.calls.categories[i]].slice(0, 10))} </ul> </div>`
+//         categoryRefs.insertAdjacentHTML('beforeend', markup);
+//         data.renderedCategories.push(data.calls.categories[i]);  
+//         runSlider(i);    
+// };
+
+// export const createСategories = () => {
+//     for (let i = 0; i < 4; i += 1) {
+//         createMarkup(i);
+//     }
+//     runLoader();
+// }
+
+
+// console.log(data.calls.specificCategory);
+// =============================================================================
+// const getСategory = async () => { 
+//     return await fetch('https://callboard-backend.herokuapp.com/call/specific/work')
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data);
+//     })
+// }
+// getСategory();
+// ===============================================================================
+
+
+// update========================================================
+
 import './styles.css';
 import { data } from "../../../data/data";
 import categoryTemplate from './template/category.hbs';
 import { runLoader } from '../../loader/index';
+import { openByCategory } from '../categories-list/index';
 
 
 export const runSlider = (i) => {
@@ -13,8 +69,8 @@ export const runSlider = (i) => {
             speed: 500,
             easing: 'ease',
             infinite: true,
-            autoplay: true,
-            autoplaySpeed: 3500, 
+            // autoplay: true,
+            // autoplaySpeed: 3500, 
         });
 };
 
@@ -22,12 +78,24 @@ export const createMarkup = (i) => {
 
     const categoryRefs = document.querySelector('.container-categories');
     const markup = `<div class="category visible"><div class="category-header"><h2 class="category-title">${data.calls.russianCategories[i]}
-            </h2><a href="" class="category-link" data-category="category">Смотреть все</a></div>
+            </h2><a href="#" class="category-link" id="${data.calls.categories[i]}">Смотреть все</a></div>
             <ul class="category-list sliders sliders-slick${i}"> 
             ${categoryTemplate(data.calls.specificCategory[data.calls.categories[i]].slice(0, 10))} </ul> </div>`
         categoryRefs.insertAdjacentHTML('beforeend', markup);
         data.renderedCategories.push(data.calls.categories[i]);  
-        runSlider(i);    
+    runSlider(i);    
+    
+
+    const linkCategoryRef = document.getElementById(`${data.calls.categories[i]}`);
+
+    linkCategoryRef.addEventListener('click', () => { 
+         const byCategoryRef = document.querySelector('.main');
+        openByCategory(data.calls.specificCategory[data.calls.categories[i]]);
+        byCategoryRef.style.display = 'none';
+    });
+
+
+
 };
 
 export const createСategories = () => {
@@ -38,7 +106,10 @@ export const createСategories = () => {
 }
 
 
-console.log(data.calls.specificCategory);
+// console.log(data.calls.specificCategory);
+
+
+
 // =============================================================================
 // const getСategory = async () => { 
 //     return await fetch('https://callboard-backend.herokuapp.com/call/specific/work')
@@ -49,6 +120,8 @@ console.log(data.calls.specificCategory);
 // }
 // getСategory();
 // ===============================================================================
+
+
 
 
 
