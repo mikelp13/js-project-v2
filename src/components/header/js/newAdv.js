17cd3a82063d9254bc692f99a6a9289958e0de47
Tatiana <taulru@yandex.com>
2020-12-27 12:38:21 +0100

    added mobile button new adv

--- src/components/header/js/newAdv.js
+++ src/components/header/js/newAdv.js
@@ -2,27 +2,31 @@ import { createNewAdv } from '../../ads/newAddForm/newAddForm';
 import { modalBackDrop } from '../../modal/modalBackDrop';
 import newAddForm from '../../ads/newAddForm/newAddForm.hbs';
 import { regitsrationUser } from '../../auth/auth'
-import { data } from '../../../data/data';
 
+export const newAdv = () => {
 
-const buttonNewAdv = document.querySelector('.header__create-ad-btn');
-let registered;
+    const buttonNewAdv = document.querySelector('.header__create-ad-btn');
+    const buttonNewAdvMobile = document.querySelector('.main__create-ad-btn');
 
-const createAdv = () => {
-    modalBackDrop(newAddForm());
-    console.log('Make your new adv');
-    createNewAdv();
+    let registered;
+
+    const createAdv = () => {
+        modalBackDrop(newAddForm());
+        console.log('Make your new adv');
+        createNewAdv();
+    }
+
+    const goToRegistrationForm = (e) => {
+        console.log('You need to fill the registration form');
+        regitsrationUser(e);
+    }
+
+    const checkRegistered = (e) => {
+        registered = localStorage.getItem('accessToken');
+
+        registered ? createAdv() : goToRegistrationForm(e);
+    }
+
+    buttonNewAdv.addEventListener('click', checkRegistered)
+    buttonNewAdvMobile.addEventListener('click', checkRegistered)
 }
-
-const goToRegistrationForm = (e) => {
-    console.log('You need to fill the registration form');
-    regitsrationUser(e);
-}
-
-const checkRegistered = (e) => {
-    registered = localStorage.getItem('accessToken');
-
-    registered ? createAdv() : goToRegistrationForm(e);
-}
-
-buttonNewAdv.addEventListener('click', checkRegistered)
